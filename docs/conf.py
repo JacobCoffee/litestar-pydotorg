e7dd06f779519e8ea96fceb69ac2b7ef3e37739d
Jacob Coffee <jacob@z7x.org>
2025-11-30 20:05:17 -0600

    fix(docs): exclude internal directory from Sphinx builds

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "internal"]
 
 source_suffix = {
     ".rst": "restructuredtext",
